5e6031ab1f7947aee4fff93350bdf4047e58e119
ryan-9138 <ryanandaya9138@gmail.com>
2023-11-30 16:05:30 +0000

    initial commit

--- employee.py
+++ employee.py
@@ -1,31 +1,71 @@
 """Employee pay calculator."""
 """ENTER YOUR SOLUTION HERE!"""
-
 class Employee:
-    def __init__(self, name):
+    def __init__(self, name, commision, contract):
         self.name = name
+        self.commision = commision
+        self.contract = contract
 
     def get_pay(self):
         pass
 
     def __str__(self):
         return self.name
+    
+class Salary(Employee):
+    def __init__(self, name, salary, commision, contract):
+        super().__init__(name, commision, contract)
+        self.salary = salary
+        
+
+    def get_pay(self):
+        if self.commision and self.contract == 0:
+            return self.salary
+        else:
+            return self.salary + (self.contract*self.commision)
+    
+    def __str__(self):
+        if self.commision and self.contract == 0:
+            return self.name,"works on a monthly salary of "+self.salary+". Their total pay is "+self.salary
+        else:
+            return self.name,"works on a monthly salary of "+self.salary+" and receives a commision for "+self.contract+" contract(s) at "+self.commision+"/contract. Their total pay is "+self.salary
+
+class Contract(Employee):
+    def __init__(self, name, rate, hours, commision, contract):
+        super().__init__(name, commision, contract)
+        self.rate = rate
+        self.hours = hours
+
+    def get_pay(self):
+        if self.commision and self.contract == 0:
+            return self.rate*self.hours
+        else:
+            return self.rate*self.hours + self.commision*self.contract
+    
+    def __str__(self):
+        if self.commision and self.contract == 0:
+            return self.name,"works on a contract of "+self.hours+" hours at "+self.rate+"/hour. Their total pay is "+self.rate*self.hours
+        else:
+            return self.name+" works on a contract of "+self.hours+" hours at "+self.rate+"/hour and receives a commission for "+self.contract+" contract(s) at "+self.commision+"/contract. Their total pay is "+(self.rate*self.hours + self.contract*self.commision)
+    
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie')
+billie = Salary('Billie', 4000, 0, 0)
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie')
+charlie = Contract('Charlie', 25, 100, 0, 0)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee')
+renee = Salary('Renee', 3000, 200, 4)
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan')
+jan = Contract('Jan', 25, 150, 220, 3)
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie')
+robbie = Salary('Robbie', 2000, 1500, 1)
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel')
+ariel = Contract('Ariel', 30, 120, 600, 1)
+
+
